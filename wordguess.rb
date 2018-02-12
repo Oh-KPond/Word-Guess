c2298e08415f3f2971fbadf99888d5d0c44825b4
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-02-12 15:22:13 -0800

    set up skeleton

---
+++ wordguess.rb
@@ -0,0 +1,16 @@
+class Word
+  def initialize
+  end
+end
+
+class Letter
+  def initialize(argument)
+    @argument = argument
+  end
+end
+
+class Art
+  def initialize(argument)
+    @argument = argument
+  end
+end
